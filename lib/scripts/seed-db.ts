2000d6296da3b0fd0fc6044a5312e8c6f4f89169
Tomas Kislan <kislan.tomas@gmail.com>
2023-10-17 10:17:41 +0000

    Update seed-db script with fakerjs

--- lib/scripts/seed-db.ts
+++ lib/scripts/seed-db.ts
@@ -1,27 +1,47 @@
 import { createDB } from '../../src/lib/db'
+import { faker } from '@faker-js/faker'
 
 async function seedDB() {
   console.log('Seeding database...')
 
   const db = createDB()
 
+  await db.deleteFrom('productsReviews').execute()
   await db.deleteFrom('products').execute()
 
-  await db
-    .insertInto('products')
-    .values([
-      {
-        name: 'iPhone 12',
-        description: 'Apple IPHONE',
-        price: 700,
-      },
-      {
-        name: 'iPhone 11',
-        description: 'Apple old IPHONE',
-        price: 600,
-      },
-    ])
-    .execute()
+  const products = []
+
+  for (let i = 0; i < 100; i++) {
+    products.push({
+      name: faker.commerce.productName(),
+      description: faker.commerce.productDescription(),
+      price: parseFloat(faker.commerce.price()),
+    })
+  }
+
+  // const createdProducts = await db.insertInto('products').values(products).returning('id').execute()
+  await db.insertInto('products').values(products).execute()
+  const createdProducts = await db.selectFrom('products').select('id').execute()
+
+  const reviews = []
+
+  for (const createdProduct of createdProducts) {
+    console.log(createdProduct.id)
+
+    const nReviews = faker.number.int({ min: 0, max: 5 })
+
+    for (let i = 0; i < nReviews; i++) {
+      reviews.push({
+        productId: createdProduct.id,
+        rating: faker.number.int({ min: 1, max: 5 }),
+        content: faker.lorem.sentences({ min: 1, max: 5 }),
+      })
+    }
+  }
+
+  if (reviews.length > 0) {
+    await db.insertInto('productsReviews').values(reviews).execute()
+  }
 
   console.log('Done')
 }
